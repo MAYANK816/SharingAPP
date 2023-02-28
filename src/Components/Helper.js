fd992d4e75d5f4a5d52d9874eb748e2c3b91de50
Mayank Khatri <63389169+MAYANK816@users.noreply.github.com>
2023-02-28 11:19:27 +0530

    Update Helper.js

--- src/Components/Helper.js
+++ src/Components/Helper.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io.connect("https://sharingapp-22.herokuapp.com/");
+const socket = io.connect("https://sharing-app-backend.vercel.app/");
 export default socket;
